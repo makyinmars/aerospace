824d9e696b815595a42a682f302e50dc22dca191
Franklin <franklinjaradev@gmail.com>
2024-08-20 08:50:49 -0400

    size font

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 11
+config.font_size = 12
 
 config.enable_tab_bar = false
 
